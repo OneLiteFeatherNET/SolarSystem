c8c5563833a5d09492fff905565bf0e034507337
TheMeinerLp <p.glanz@madfix.me>
2023-01-28 12:49:17 +0100

    Add database structure for planets

--- plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/database/plant.kt
+++ plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/database/plant.kt
@@ -1,55 +1,66 @@
 package dev.themeinerlp.solarsystem.api.database
 
+import org.bukkit.Difficulty
+import org.bukkit.GameMode
+import org.bukkit.World.Environment
+import org.bukkit.WorldCreator
+import org.bukkit.WorldType
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 
-object PlanetTables: IntIdTable() {
+object PlanetTables : IntIdTable() {
     val name = text("name").index()
-    val difficulty = integer("difficulty")
-    val environment = integer("environment")
-    val worldType = varchar("worldtype", 255).index()
+    val difficulty = enumeration<Difficulty>("difficulty").default(Difficulty.EASY)
+    val environment = enumeration<Environment>("environment").default(Environment.NORMAL)
+    val worldType = enumeration<WorldType>("worldtype").default(WorldType.NORMAL)
     val seed = long("seed").default(0)
     val generator = text("generator").nullable().index()
     val alias = text("alias").nullable().index()
-    val monsterSpawning = bool("monsterSpawning")
+    val monsterSpawning = bool("monsterSpawning").default(true)
     val pvp = bool("pvp").default(true)
     val hidden = bool("hidden").default(false)
     val weather = bool("weather").default(true)
     val hunger = bool("hunger").default(true)
-    val autoHeal = bool("autoHeal")
-    val adjustSpawn = bool("adjustSpawn")
-    val autoLoad = bool("autoLoad")
-    val bedRespawn = bool("bedRespawn")
-    val allowFlight = bool("allowFlight")
-    val time = long("time")
-    val playerLimit = long("playerLimit")
+    val autoHeal = bool("autoHeal").default(true)
+    val adjustSpawn = bool("adjustSpawn").default(true)
+    val autoLoad = bool("autoLoad").default(true)
+    val bedRespawn = bool("bedRespawn").default(true)
+    val allowFlight = bool("allowFlight").default(false)
+    val time = long("time").default(0)
+    val playerLimit = integer("playerLimit").default(-1)
     val respawnPlanet = reference("respawnPlanet", PlanetTables).nullable()
-    val gamemode = integer("gamemode")
+    val gamemode = enumeration<GameMode>("gamemode").default(GameMode.SURVIVAL)
 }
 
-class PlantEntity(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<PlantEntity>(PlanetTables)
-    val name by PlanetTables.name
-    val difficulty by PlanetTables.difficulty
-    val environment by PlanetTables.environment
-    val worldType by PlanetTables.worldType
-    val seed by PlanetTables.seed
-    val generator by PlanetTables.generator
-    val alias by PlanetTables.alias
-    val monsterSpawning by PlanetTables.monsterSpawning
-    val pvp by PlanetTables.pvp
-    val hidden by PlanetTables.hidden
-    val weather by PlanetTables.weather
-    val hunger by PlanetTables.hunger
-    val autoHeal by PlanetTables.autoHeal
-    val adjustSpawn by PlanetTables.adjustSpawn
-    val autoLoad by PlanetTables.autoLoad
-    val bedRespawn by PlanetTables.bedRespawn
-    val allowFlight by PlanetTables.allowFlight
-    val time by PlanetTables.time
-    val playerLimit by PlanetTables.playerLimit
-    val respawnPlanet by PlantEntity optionalReferrersOn PlanetTables.respawnPlanet
-    val gamemode by PlanetTables.gamemode
+class PlanetEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<PlanetEntity>(PlanetTables)
+
+    var name by PlanetTables.name
+    var difficulty by PlanetTables.difficulty
+    var environment by PlanetTables.environment
+    var worldType by PlanetTables.worldType
+    var seed by PlanetTables.seed
+    var generator by PlanetTables.generator
+    var alias by PlanetTables.alias
+    var monsterSpawning by PlanetTables.monsterSpawning
+    var pvp by PlanetTables.pvp
+    var hidden by PlanetTables.hidden
+    var weather by PlanetTables.weather
+    var hunger by PlanetTables.hunger
+    var autoHeal by PlanetTables.autoHeal
+    var adjustSpawn by PlanetTables.adjustSpawn
+    var autoLoad by PlanetTables.autoLoad
+    var bedRespawn by PlanetTables.bedRespawn
+    var allowFlight by PlanetTables.allowFlight
+    var time by PlanetTables.time
+    var playerLimit by PlanetTables.playerLimit
+    val respawnPlanet by PlanetEntity optionalReferrersOn PlanetTables.respawnPlanet
+    var gamemode by PlanetTables.gamemode
+
+    val worldCreator: WorldCreator
+        get() {
+            return WorldCreator.name(name).generator(generator).environment(environment).seed(seed).type(worldType)
+        }
 }
