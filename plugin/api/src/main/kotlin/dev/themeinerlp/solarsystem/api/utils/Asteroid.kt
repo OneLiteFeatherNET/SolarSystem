92c6e7ac5a2456873d4e3d5f9151ed8f5b3a6cd3
TheMeinerLp <p.glanz@madfix.me>
2023-01-28 12:47:51 +0100

    Add a wrapper for player

---
+++ plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/utils/Asteroid.kt
@@ -0,0 +1,17 @@
+package dev.themeinerlp.solarsystem.api.utils
+
+import dev.themeinerlp.solarsystem.api.service.SolarService
+import dev.themeinerlp.solarsystem.api.world.Planet
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+data class Asteroid<T>(
+    val commandSender: CommandSender,
+    val player: Player,
+    val service: SolarService<T>
+) {
+    val currentPlanet: Planet<T>
+        get() {
+            return service.getPlanetByName(player.world.name)
+        }
+}
