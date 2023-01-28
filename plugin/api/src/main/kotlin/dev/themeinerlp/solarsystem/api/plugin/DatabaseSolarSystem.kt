b946f8bd3a5c3e05fcac74ce7d76dfbf3b78cfc2
TheMeinerLp <p.glanz@madfix.me>
2023-01-28 12:48:25 +0100

    Add database integration

--- plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/plugin/DatabaseSolarSystem.kt
+++ plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/plugin/DatabaseSolarSystem.kt
@@ -1,24 +1,92 @@
 package dev.themeinerlp.solarsystem.api.plugin
 
+import cloud.commandframework.annotations.AnnotationParser
+import cloud.commandframework.arguments.parser.ParserParameters
+import cloud.commandframework.arguments.parser.StandardParameters
+import cloud.commandframework.bukkit.CloudBukkitCapabilities
+import cloud.commandframework.execution.CommandExecutionCoordinator
+import cloud.commandframework.meta.CommandMeta
+import cloud.commandframework.paper.PaperCommandManager
 import com.typesafe.config.ConfigFactory
 import com.zaxxer.hikari.HikariDataSource
+import dev.themeinerlp.solarsystem.api.commands.CreateCommand
+import dev.themeinerlp.solarsystem.api.commands.DeleteCommand
+import dev.themeinerlp.solarsystem.api.commands.TeleportCommand
 import dev.themeinerlp.solarsystem.api.config.SQLConfig
 import dev.themeinerlp.solarsystem.api.database.PlanetTables
+import dev.themeinerlp.solarsystem.api.database.PlanetEntity
+import dev.themeinerlp.solarsystem.api.parser.PlanetParser
+import dev.themeinerlp.solarsystem.api.utils.Asteroid
 import dev.themeinerlp.solarsystem.api.utils.CONFIG_FILE_NAME
+import io.leangen.geantyref.TypeToken
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.hocon.Hocon
 import kotlinx.serialization.hocon.decodeFromConfig
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlLogger
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.DatabaseConfig
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.statements.StatementContext
 import org.jetbrains.exposed.sql.statements.expandArgs
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.lang.IllegalArgumentException
 import java.nio.file.Files
+import java.util.function.Function
 import java.util.logging.Logger
 import kotlin.io.path.reader
 
 abstract class DatabaseSolarSystem<T> : JavaPlugin(), SolarSystem<T> {
+
+    private var paperCommandManager: PaperCommandManager<Asteroid<T>>? = null
+    private var annotationParser: AnnotationParser<Asteroid<T>>? = null
+
+    protected fun createCommandSystem() {
+        paperCommandManager = PaperCommandManager(
+            this,
+            CommandExecutionCoordinator.simpleCoordinator(),
+            {
+                Asteroid(player = it as Player, service = getSolarService(), commandSender = it)
+            },
+            {
+                it.commandSender
+            }
+        )
+        if (paperCommandManager!!.hasCapability(CloudBukkitCapabilities.BRIGADIER)) {
+            paperCommandManager!!.registerBrigadier()
+            logger.info("Brigadier support enabled")
+        }
+        if (paperCommandManager!!.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
+            paperCommandManager!!.registerAsynchronousCompletions()
+            logger.info("Asynchronous completions enabled")
+        }
+        val commandMetaFunction = Function<ParserParameters, CommandMeta> { p: ParserParameters ->
+            CommandMeta.simple().with(
+                CommandMeta.DESCRIPTION,
+                p.get(StandardParameters.DESCRIPTION, "No description")
+            ).build()
+        }
+        val type = object : TypeToken<Asteroid<T>>() {}
+        annotationParser = AnnotationParser(
+            paperCommandManager!!,
+            type,
+            commandMetaFunction
+        )
+    }
+
+    protected fun registerCommands() {
+        if (annotationParser != null) {
+            annotationParser!!.parse(PlanetParser())
+
+            annotationParser!!.parse(TeleportCommand())
+            annotationParser!!.parse(CreateCommand())
+            annotationParser!!.parse(DeleteCommand())
+        }
+    }
 
     @OptIn(ExperimentalSerializationApi::class)
     protected fun readConfig(): SQLConfig {
@@ -44,6 +112,12 @@ abstract class DatabaseSolarSystem<T> : JavaPlugin(), SolarSystem<T> {
             SchemaUtils.createMissingTablesAndColumns(
                 PlanetTables
             )
+        }
+    }
+
+    protected fun autoLoadPlanets() = transaction {
+        PlanetEntity.find { PlanetTables.autoLoad eq true }.forEach {
+            getSolarService().loadPlanetByName(it.name)
         }
     }
 
