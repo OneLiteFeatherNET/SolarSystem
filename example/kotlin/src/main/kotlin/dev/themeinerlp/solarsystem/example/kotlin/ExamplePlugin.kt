73dae9a4d0fb88821e9a04311c3eb7e8d55600f3
TheMeinerLp <p.glanz@madfix.me>
2023-01-28 12:45:17 +0100

    Add example api usage

---
+++ example/kotlin/src/main/kotlin/dev/themeinerlp/solarsystem/example/kotlin/ExamplePlugin.kt
@@ -0,0 +1,15 @@
+package dev.themeinerlp.solarsystem.example.kotlin
+
+import dev.themeinerlp.solarsystem.api.plugin.SolarSystem
+import org.bukkit.World
+import org.bukkit.plugin.java.JavaPlugin
+
+class ExamplePlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        if (server.pluginManager.isPluginEnabled("SolarSystem")) {
+            val solarSystemPlugin = server.pluginManager.getPlugin("SolarSystem") as SolarSystem<World>
+
+        }
+    }
+}
