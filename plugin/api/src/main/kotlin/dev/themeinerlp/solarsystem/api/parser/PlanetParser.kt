f619ce8e5ee9de930d651d5c5ab60568555fbd9d
TheMeinerLp <p.glanz@madfix.me>
2023-01-28 12:48:42 +0100

    Add planet parser for command system

---
+++ plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/parser/PlanetParser.kt
@@ -0,0 +1,25 @@
+package dev.themeinerlp.solarsystem.api.parser
+
+import cloud.commandframework.annotations.parsers.Parser
+import cloud.commandframework.annotations.suggestions.Suggestions
+import cloud.commandframework.context.CommandContext
+import dev.themeinerlp.solarsystem.api.database.PlanetEntity
+import dev.themeinerlp.solarsystem.api.utils.Asteroid
+import org.bukkit.World
+import org.bukkit.util.StringUtil
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.Queue
+
+class PlanetParser {
+
+    @Parser(name = "planet", suggestions = "planets")
+    fun planetParser(asteroid: CommandContext<Asteroid<World>>, input: Queue<String>) = transaction {
+        val name = input.remove()
+        return@transaction asteroid.sender.service.getPlanetByName(name)
+    }
+
+    @Suggestions(value = "planets")
+    fun platenSuggestions(asteroid: CommandContext<Asteroid<World>>, input: String): List<String> = transaction {
+        return@transaction StringUtil.copyPartialMatches(input, PlanetEntity.all().map { it.name }, mutableListOf())
+    }
+}
