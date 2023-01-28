c03c7ce55c5b0b413e8c4775752a7ae090b6460d
TheMeinerLp <p.glanz@madfix.me>
2023-01-28 12:51:21 +0100

    Add bukkit plugin structure

---
+++ plugin/bukkit/build.gradle.kts
@@ -0,0 +1,45 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+plugins {
+    kotlin("jvm") version "1.8.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("xyz.jpenilla.run-paper") version "2.0.1"
+}
+
+group = "dev.themeinerlp"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    api(project(":plugin:api"))
+
+    bukkitLibrary("com.h2database:h2:2.1.214")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.19.3")
+    }
+}
+
+bukkit {
+    name = "SolarSystem"
+    load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
+    main = "dev.themeinerlp.solarsystem.bukkit.BukkitSolarSystem"
+    apiVersion = "1.13"
+    authors = listOf("TheMeinerLP")
+
+    defaultPermission = net.minecrell.pluginyml.bukkit.BukkitPluginDescription.Permission.Default.OP
+}
