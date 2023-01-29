28e86a36acb6df8514988bf5d81a0228cd9b5fe7
TheMeinerLp <p.glanz@madfix.me>
2023-01-29 01:49:18 +0100

    Add some planet methods to remove,unload,add

--- plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/service/SolarService.kt
+++ plugin/api/src/main/kotlin/dev/themeinerlp/solarsystem/api/service/SolarService.kt
@@ -1,6 +1,7 @@
 package dev.themeinerlp.solarsystem.api.service
 
 import dev.themeinerlp.solarsystem.api.world.Planet
+import org.bukkit.World.Environment
 
 interface SolarService<T> {
 
@@ -8,7 +9,11 @@ interface SolarService<T> {
 
     fun clonePlanet(world: Planet<T>, clonedWorldName: String)
 
+    fun addPlanet(name: String, environment: Environment, generator: String?, useSpawnAdjust: Boolean)
+
     fun deletePlanet(world: Planet<T>): Boolean
+    fun removePlanet(world: Planet<T>): Boolean
+    fun unloadPlanet(world: Planet<T>): Boolean
 
     fun loadPlanetByName(name: String): Planet<T>
 
@@ -16,5 +21,7 @@ interface SolarService<T> {
 
     fun isSolarPlanet(name: String): Boolean
     fun isSolarPlanet(world: Planet<T>): Boolean
+
+    fun getPlanets(): List<Planet<T>>
 
 }
