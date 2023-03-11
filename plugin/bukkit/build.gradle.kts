6cc413c9970c1f0b2991f5a93b5326daeef36334
TheMeinerLp <p.glanz@madfix.me>
2023-03-12 00:19:11 +0100

    Bump bukkit yml generator plugin

--- plugin/bukkit/build.gradle.kts
+++ plugin/bukkit/build.gradle.kts
@@ -3,7 +3,7 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("xyz.jpenilla.run-paper") version "2.0.1"
     id("io.papermc.hangar-publish-plugin") version "0.0.3"
