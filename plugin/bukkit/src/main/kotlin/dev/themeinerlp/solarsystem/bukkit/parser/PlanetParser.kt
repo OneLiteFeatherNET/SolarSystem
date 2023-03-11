444b8eb9c391d6f229530107f5f6dd743548df5e
TheMeinerLp <p.glanz@madfix.me>
2023-03-11 23:40:31 +0100

    Add command parser and suggestions

--- plugin/bukkit/src/main/kotlin/dev/themeinerlp/solarsystem/bukkit/parser/PlanetParser.kt
+++ plugin/bukkit/src/main/kotlin/dev/themeinerlp/solarsystem/bukkit/parser/PlanetParser.kt
@@ -1,15 +1,16 @@
-package dev.themeinerlp.solarsystem.api.parser
+package dev.themeinerlp.solarsystem.bukkit.parser
 
 import cloud.commandframework.annotations.parsers.Parser
 import cloud.commandframework.context.CommandContext
 import dev.themeinerlp.solarsystem.api.utils.Asteroid
+import org.bukkit.World
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
 class PlanetParser {
 
     @Parser(name = "planet", suggestions = "planets")
-    fun planetParser(asteroid: CommandContext<Asteroid>, input: Queue<String>) = transaction {
+    fun planetParser(asteroid: CommandContext<Asteroid<World>>, input: Queue<String>) = transaction {
         val name = input.remove()
         return@transaction asteroid.sender.service.getPlanetByName(name)
     }
--- plugin/bukkit/src/main/kotlin/dev/themeinerlp/solarsystem/bukkit/suggetions/PlantSuggestion.kt
+++ plugin/bukkit/src/main/kotlin/dev/themeinerlp/solarsystem/bukkit/suggetions/PlantSuggestion.kt
@@ -1,16 +1,17 @@
-package dev.themeinerlp.solarsystem.api.suggetions
+package dev.themeinerlp.solarsystem.bukkit.suggetions
 
 import cloud.commandframework.annotations.suggestions.Suggestions
 import cloud.commandframework.context.CommandContext
 import dev.themeinerlp.solarsystem.api.database.PlanetEntity
 import dev.themeinerlp.solarsystem.api.utils.Asteroid
 import org.bukkit.Bukkit
+import org.bukkit.World
 import org.bukkit.util.StringUtil
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class PlantSuggestion {
     @Suggestions(value = "planets")
-    fun platenSuggestions(asteroid: CommandContext<Asteroid>, input: String): List<String> = transaction {
+    fun platenSuggestions(asteroid: CommandContext<Asteroid<World>>, input: String): List<String> = transaction {
         return@transaction StringUtil.copyPartialMatches(
             input,
             PlanetEntity.all().filter { Bukkit.getWorld(it.name) != null }.map { it.name },
