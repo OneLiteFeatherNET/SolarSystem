90c739c32d90020768357728943159d912ebea2b
TheMeinerLp <p.glanz@madfix.me>
2023-03-12 01:13:53 +0100

    Fix mapping

--- plugin/bukkit/src/main/kotlin/dev/themeinerlp/solarsystem/bukkit/mapper/WorldTypeExtension.kt
+++ plugin/bukkit/src/main/kotlin/dev/themeinerlp/solarsystem/bukkit/mapper/WorldTypeExtension.kt
@@ -3,9 +3,9 @@ package dev.themeinerlp.solarsystem.bukkit.mapper
 import dev.themeinerlp.solarsystem.api.wrapper.world.WorldType
 
 fun WorldType.toBukkit(): org.bukkit.WorldType {
-    return org.bukkit.WorldType.valueOf(this.bukkitValue)
+    return org.bukkit.WorldType.valueOf(this.name)
 }
 
 fun org.bukkit.WorldType.toSolar(): WorldType {
-    return WorldType.values().first { it.bukkitValue == this.getName() }
+    return WorldType.valueOf(this.name)
 }
