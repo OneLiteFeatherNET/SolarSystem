e4c012f3bb82e58a96d5a952d7216e16b8a050d2
TheMeinerLP <p.glanz@madfix.me>
2023-03-19 22:27:12 +0100

    Update to paper 1.19.4

--- plugin/bukkit/build.gradle.kts
+++ plugin/bukkit/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
 tasks {
     runServer {
-        minecraftVersion("1.19.3")
+        minecraftVersion("1.19.4")
     }
     shadowJar {
         archiveFileName.set("${rootProject.name}.${archiveExtension.getOrElse("jar")}")
